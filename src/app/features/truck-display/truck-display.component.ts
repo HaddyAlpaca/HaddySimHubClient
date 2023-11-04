b976156b00fe47f26eaf6cc6b53080ed1fde2da7
Haddy <126945533+HaddyAlpaca@users.noreply.github.com>
2023-11-04 07:48:03 +0100

    Truck display redesign WIP

--- src/app/features/truck-display/truck-display.component.ts
+++ src/app/features/truck-display/truck-display.component.ts
@@ -1,21 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { filter, tap } from 'rxjs';
 import { TelemetryService } from 'src/app/shared/services';
 import { TelemetryType } from 'src/app/shared/services/telemetry.service';
-import { GearRange, TruckData } from './data';
+import { TruckData } from './truck-data';
 
 
 @UntilDestroy()
 @Component({
   selector: 'app-truck-display',
   templateUrl: './truck-display.component.html',
-  styleUrls: ['./truck-display.component.css']
+  styleUrls: ['./truck-display.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TruckDisplayComponent implements OnInit {
-  readonly GearRange = GearRange;
-
-  private _data: TruckData = this.getDefaultData();
+  private _data: TruckData = new TruckData();
   get data(): TruckData{
     return this._data;
   }
@@ -30,34 +29,5 @@ export class TruckDisplayComponent implements OnInit {
       filter(update => update.Type === TelemetryType.Truck),
       tap(update => this._data = update.Data as TruckData),
       untilDestroyed(this)).subscribe();
-  }
-
-  private getDefaultData(): TruckData {
-    return {
-      destination: '',
-      distanceRemaining: 0,
-      timeRemaining: 0,
-      timeRemainingIrl: 0,
-      time: new Date(),
-      restTimeRemaining: 0,
-      restTimeRemainingIrl: 0,
-      fuelPercentage: 0,
-      fuelDistance: 0,
-      jobTimeRemaining: 0,
-      jobTimeRemainingIrl: 0,
-      jobIncome: 0,
-      speed: 0,
-      speedLimit: 0,
-      rpm: 0,
-      rpmMax: 0,
-      cruiseControlOn: false,
-      cruiseControlSpeed: 0,
-      gear: 0,
-      gearRange: GearRange.Low,
-      lowBeamOn: false,
-      highBeamOn: false,
-      parkingBrakeOn: false,
-      batteryWarningOn: false
-    };
   }
 }
