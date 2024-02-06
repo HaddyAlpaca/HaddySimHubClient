14d640508986d39cd2b33d93dad487d9cae9378f
Haddy <126945533+HaddyAlpaca@users.noreply.github.com>
2024-02-06 18:15:39 +0100

    Improve clock service

--- src/app/services/clock.service.ts
+++ src/app/services/clock.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, signal } from '@angular/core';
-import { UntilDestroy } from '@ngneat/until-destroy';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { tap, timer } from 'rxjs';
 
 @UntilDestroy()
@@ -13,6 +13,7 @@ export class ClockService {
   constructor() {
     timer(0, 1000).pipe(
       tap(() => this._time.set(new Date())),
+      untilDestroyed(this),
     ).subscribe();
   }
 }
